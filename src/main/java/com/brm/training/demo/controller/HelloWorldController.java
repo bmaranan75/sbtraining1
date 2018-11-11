4633132788ee5ff40adecc852c63c10f754d96f1
brandonmaranan <brandon.maranan@gmail.com>
2018-11-11 08:14:06 -0800

    Adding controller.

--- src/main/java/com/brm/training/demo/controller/HelloWorldController.java
+++ src/main/java/com/brm/training/demo/controller/HelloWorldController.java
@@ -1,4 +1,12 @@
 package com.brm.training.demo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HelloWorldController {
+
+    @GetMapping(path="/customer/{customer_id}/profile")
+    public ResponseEntity<String> getCustomerProfile(){
+        return null;
+    }
 }
