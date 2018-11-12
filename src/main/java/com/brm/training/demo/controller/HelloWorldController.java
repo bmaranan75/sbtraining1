a075980a880eaad3a150c18a442607031fb71bc9
brandonmaranan <brandon.maranan@gmail.com>
2018-11-11 18:32:11 -0800

    Added PatchMapping to master.

--- src/main/java/com/brm/training/demo/controller/HelloWorldController.java
+++ src/main/java/com/brm/training/demo/controller/HelloWorldController.java
@@ -2,11 +2,17 @@ package com.brm.training.demo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 
 public class HelloWorldController {
 
     @GetMapping(path="/customer/{customer_id}/profile")
     public ResponseEntity<String> getCustomerProfile(){
+        return null;
+    }
+
+    @PatchMapping(value="/customer/{customer_id/profile")
+    public void ResponseEntity<String> updateProfile(){
         return null;
     }
 }
